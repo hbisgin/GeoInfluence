b05912b85bacb7be3fa7d97026b62853fd709b76
oztalha <toz@gmu.edu>
2015-04-22 20:48:04 -0400

    scrapers

---
+++ scrapers/govFollowers4.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Get user profiles of followers of governors
+
+Created on Wed Apr 22 18:25:21 2015
+
+@author: Talha
+"""
+import json
+import os,sys
+os.chdir(r'C:\Users\Talha\Documents\WinPython3\projects\GeoInfluence\utilities')
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+from auth import keys
+from utilities.twhelper import oauth_login, make_twitter_request
+
+twitter_api = oauth_login(keys.user['manqili'])
+
+# filenames sorted by their size in ascending order
+files = sorted(os.listdir('../raw_data'), key=lambda x: os.path.getsize('../raw_data/'+x))
+
+for filename in files[49:]:
+    # read in the governor's follower IDs
+    followers_file = open('../raw_data/'+filename)
+    followers = json.load(followers_file)
+    fids = followers['data']['followers']
+    gov = followers['parameters']['screen_name']
+
+    resp=[]
+    for i in range(1+len(fids)//100):
+        res = make_twitter_request(twitter_api.users.lookup, user_id=fids[100*i:100*(i+1)])
+        resp.extend(res)
+    
+    with open('../followers/'+filename, 'w') as outfile:
+        json.dump(resp, outfile)
